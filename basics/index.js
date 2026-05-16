21f8446a9056727bf10dfb7596979b56dc8bba5f
vinay-here <krvinay785@gmail.com>
2026-05-16 21:10:39 +0530

    string obj arr

--- basics/index.js
+++ basics/index.js
@@ -1,6 +1,39 @@
+
+/*
 let firstName = 7;
 let lastName = 3;
 
 firstName = "vinay";
 let fullName = firstName + lastName
 console.log(fullName);
+
+*/
+  // array 
+/*
+let arr = [2,5,7,100,8]
+let sum = arr[2] + arr[4]
+console.log(sum); //15 
+console.log(arr[3]); //100
+
+ let names = ["vinay","sumit","sachin","himanshu"];
+ console.log(names[2]);
+
+ let arrOfArr = ["vinay",7,[2,4,[5,6]]];
+ console.log(arrOfArr[2][2][0]); //5
+
+ */
+
+ // /*
+
+ let obj = {
+    a:1,
+    name: "vinay",
+    boolean: true,
+    arr : [1,2,4,6], 
+    first : "ravi",
+    last: "kumar",
+ }
+ console.log(obj);
+ console.log(obj.name); // vinay 
+ console.log(obj.first+" "+obj.last); // ravi kumar 
+
