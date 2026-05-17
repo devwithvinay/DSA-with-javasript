7277961e36f99878d0e18a9fc6f3993548def3bc
vinay-here <krvinay785@gmail.com>
2026-05-17 22:41:33 +0530

    function ,if-else

--- basics/index.js
+++ basics/index.js
@@ -23,7 +23,7 @@ console.log(arr[3]); //100
 
  */
 
- // /*
+ /*
 
  let obj = {
     a:1,
@@ -36,4 +36,82 @@ console.log(arr[3]); //100
  console.log(obj);
  console.log(obj.name); // vinay 
  console.log(obj.first+" "+obj.last); // ravi kumar 
+ 
 
+ */
+
+  // function 
+
+ function greet (name){
+  console.log('Namaste, '+ name);
+  
+ }
+
+ let x = "vinay kumar";
+
+ greet(x)
+
+//  function add (num1 , num2) {
+//   console.log(num1+num2);
+//  }
+
+//  add(2,3); 
+
+ function sum (a,b){
+  let add = a + b;
+  console.log(add);
+ }
+
+//  let x = 3;
+ let y = 5;
+ let z = 9;
+
+ sum(y,z)
+
+ function square (a){
+  let result  = a*a
+  return result;
+ }
+let value = square(9)
+
+console.log(value);
+
+// if else 
+
+  function eligible (age){
+    if(age<18){
+      console.log('not eligible');
+    }else{
+      console.log('you can vote');
+    }
+  }
+
+  eligible (15) // not eligible
+  eligible(23) // you can vote
+  eligible(18) // you can vote
+
+    function eligibleToVote (age){
+
+      if (age<0){
+        console.log('Invalid age');
+      }
+    else if(age<18){
+      console.log('not eligible');
+    }else{
+      console.log('you can vote');
+    }
+  }
+
+  eligibleToVote(-1); // not eligible
+  eligibleToVote(23); // you can vote
+  eligibleToVote(18); // you can vote
+
+   function evenOdd (num){
+    if(num%2==0){
+      console.log('Even');
+    }else{
+      console.log('odd');
+    }
+   }
+
+   evenOdd(2)
