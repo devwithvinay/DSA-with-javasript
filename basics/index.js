c51a97bc7ee60c47f25a9c773cb5b707b2ed590e
vinay-here <krvinay785@gmail.com>
2026-05-26 20:16:15 +0530

    4star

--- basics/index.js
+++ basics/index.js
@@ -298,4 +298,12 @@ for (let i = 0; i<3; i++) {
  }
  
  //star pattern
- 
+
+let n = 4 ; 
+for (let i=0; i<n; i++){
+  row = ""
+  for(let j=0; j<n;j++){
+    row=row+"*";
+  }
+  console.log(row);
+}
