78d1c73f33eb9468981033c3fb5d282918cdfbc7
vinay-here <krvinay785@gmail.com>
2026-05-29 01:09:47 +0530

    binary pattern

--- basics/index.js
+++ basics/index.js
@@ -337,14 +337,14 @@ for (let i = 0; i < n; i++) {
 }
   */
 
-let n = 5;
-for (let i = 0; i < n; i++) {
-  let row = "";
-  for (let j = 0; j <= i; j++) {
-    row = row + (i + 1);
-  }
-  console.log(row);
-}
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   for (let j = 0; j <= i; j++) {
+//     row = row + (i + 1);
+//   }
+//   console.log(row);
+// }
 
 // same but remove  0
 
@@ -356,4 +356,74 @@ for (let i = 0; i < n; i++) {
 //   }
 //   console.log(row);
 // }
+
+// reverse pattern
+
+// let n =5;
+// for(let i=0; i<n;i++){
+//   let row="";
+//   for(let j=0;j<n-i;j++){
+//     row = row + (j+1)
+//   }
+//   console.log(row);
+// }
+
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   for (let j = 0; j < n - i; j++) {
+//     row = row + "*";
+//   }
+//   console.log(row);
+// }
+
+// tricky Ques
+
+// let n = 5; 
+// for(let i=0; i<n; i++){
+//   let row = "";
+ // for empty space
+//   for(let j=0; j<n-(i+1); j++){
+//     row = row + " "
+//   }
+//for stars 
+//    for(let k=0; k<i+1; k++){
+//     row= row + "*"
+//   }
+
+//   console.log(row);
+// }
+
+ // Its a 1010 problems
+// let n = 6; 
+// for(let i=0; i<n; i++){
+//   let row = "";
+//   let Switch = 1;
+//   for(let j=0; j<=i ; j++){
+//     row = row + Switch;
+//     if(Switch==1){
+//       Switch=0;
+//     }else{
+//       Switch = 1;
+//     }
+//   }
+//   console.log(row);
+// }
+
+let n = 6;
+  let Switch = 1;
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+  
+  for (let j = 0; j <= i; j++) {
+    row = row + Switch;
+    if (Switch == 1) {
+      Switch = 0;
+    } else {
+      Switch = 1;
+    }
+  }
+  console.log(row);
+}
 
